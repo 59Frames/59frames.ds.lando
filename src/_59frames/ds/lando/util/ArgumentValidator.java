c77c037f8aecf0c4af24ba26e09464f096474fca
danielseifert <info@danielseifert.ch>
2019-03-21 12:50:07 +0100

    improved security

--- src/_59frames/ds/lando/util/ArgumentValidator.java
+++ src/_59frames/ds/lando/util/ArgumentValidator.java
@@ -13,10 +13,6 @@ import java.util.LinkedHashMap;
 public class ArgumentValidator {
     private static final LinkedHashMap<String, Argument> invalidArguments = new LinkedHashMap<>();
 
-    public static boolean isInvalid(@NotNull final Argument argument, @NotNull final Constraint constraint) {
-        return !constraint.validate(argument.getKey(), argument.getValue());
-    }
-
     public static boolean validate(@NotNull final Arguments arguments, @NotNull final Constraints constraints) {
         invalidArguments.clear();
         fillInvalidArguments(arguments, constraints);
@@ -26,6 +22,10 @@ public class ArgumentValidator {
     @Contract(pure = true)
     public static LinkedHashMap<String, Argument> getInvalidArguments() {
         return invalidArguments;
+    }
+
+    private static boolean isInvalid(@NotNull final Argument argument, @NotNull final Constraint constraint) {
+        return !constraint.validate(argument.getKey(), argument.getValue());
     }
 
     private static void fillInvalidArguments(@NotNull final Arguments arguments, @NotNull final Constraints constraints) {
